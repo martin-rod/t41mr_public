d97e2ff1fe15258f214b23621f29c02b122cd272
Martin Rod <martin.rod@rod.cz>
2024-04-29 14:05:59 +0200

    CMSISDSP compile options

--- create_build_eee_arm13.sh
+++ create_build_eee_arm13.sh
@@ -14,7 +14,10 @@ echo "PRJ_DIR:${PRJ_DIR}"
 # cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V4_PJRC -D CMAKE_BUILD_TYPE=Debug
 
 # ld: warning: t41sdt has a LOAD segment with RWX permissions
-cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V4_ARM -D CMAKE_BUILD_TYPE=Debug
+# cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V4_ARM -D CMAKE_BUILD_TYPE=Debug
+
+cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+
 
 # CMSIS_TYPE = LIB_V4_PJRC, LIB_V4_ARM, SRC_V4, SRC_V6
 
