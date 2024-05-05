fbe2c4d09e02a0a4c3237e543f29a7ab4f4102df
Martin Rod <martin.rod@rod.cz>
2024-05-05 08:05:05 +0200

    Update t41eee-actions.yml 2

--- create_build_eee.sh
+++ create_build_eee.sh
@@ -10,6 +10,16 @@ rm -rf ${build_dir}/*
 export PRJ_DIR=$(pwd)
 echo "PRJ_DIR:${PRJ_DIR}"
 
-cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/teensy41.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMAKE_BUILD_TYPE=Debug
+# cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/teensy41.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMAKE_BUILD_TYPE=Debug
+
+# ld: warning: t41sdt has a LOAD segment with RWX permissions
+# cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V4_PJRC -D CMAKE_BUILD_TYPE=Debug
+
+# ld: warning: t41sdt has a LOAD segment with RWX permissions
+# cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V4_ARM -D CMAKE_BUILD_TYPE=Debug
+
+# cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V6 -D CMAKE_BUILD_TYPE=Debug
+
+cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
 
 cp script/load_to_teensy.sh ${build_dir}
