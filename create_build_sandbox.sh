fe69dbf10c873311be1dd6e5317d175ae0de9f6d
Martin Rod <martin.rod@rod.cz>
2025-09-28 11:11:45 +0200

    Sandbox build

--- create_build_sandbox.sh
+++ create_build_sandbox.sh
@@ -20,6 +20,10 @@ echo "PRJ_DIR:${PRJ_DIR}"
 
 # cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE -D CMSIS_TYPE=LIB_V6 -D CMAKE_BUILD_TYPE=Debug
 
-cmake -B ${build_dir} -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=sandbox -D STOP_AFTER_PREPROCESS=ON -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+# cmake -B ${build_dir} -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=sandbox -D STOP_AFTER_PREPROCESS=ON -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+
+#cmake -B ${build_dir} -D CMAKE_TOOLCHAIN_FILE="./cmake/teensy41.toolchain.cmake" -D T41_SRC_TYPE=sandbox -D STOP_AFTER_PREPROCESS=OFF -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+
+cmake -B ${build_dir} -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=sandbox -D STOP_AFTER_PREPROCESS=OFF -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
 
 cp script/load_to_teensy.sh ${build_dir}
