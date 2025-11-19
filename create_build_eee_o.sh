2f184c89ab9de4cb5066715f165a1af9c193f878
Martin Rod <martin.rod@rod.cz>
2025-11-19 23:37:51 +0100

    Pridani EEE orig

---
+++ create_build_eee_o.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+build_dir=build_eee_o
+
+echo "---------------------------"
+echo "Vytvarim:${build_dir}"
+echo "---------------------------"
+mkdir -p ${build_dir}
+rm -rf ${build_dir}/*
+
+export PRJ_DIR=$(pwd)
+echo "PRJ_DIR:${PRJ_DIR}"
+
+cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE_O -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+
+cp script/load_to_teensy.sh ${build_dir}
