17c5ab1ff7c8d112fe4384ffd5b2f7a54f299300
Martin Rod <martin.rod@rod.cz>
2025-12-05 19:00:49 +0100

    Add toolchain file

--- create_build_eee_o.sh
+++ create_build_eee_o.sh
@@ -10,6 +10,6 @@ rm -rf ${build_dir}/*
 export PRJ_DIR=$(pwd)
 echo "PRJ_DIR:${PRJ_DIR}"
 
-cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm13.toolchain.cmake" -D T41_SRC_TYPE=EEE_O -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
+cmake -B $build_dir -D CMAKE_TOOLCHAIN_FILE="./cmake/arm14.toolchain.cmake" -D T41_SRC_TYPE=EEE_O -D CMSIS_TYPE=SRC_V6 -D CMAKE_BUILD_TYPE=Debug
 
 cp script/load_to_teensy.sh ${build_dir}
